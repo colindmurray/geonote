6cf70835a9dfac71f314ed18fe381bf422b00c02
majisto <majisto@gmail.com>
2015-04-21 16:03:05 -0500

    MainActivity works, but map logic not put into fragment.  Still ugly and need to figure out colors.

---
+++ app/src/main/java/info/geopost/geopost/MainActivity.java
@@ -0,0 +1,227 @@
+package info.geopost.geopost;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import com.rey.material.app.ToolbarManager;
+import com.rey.material.demo.CustomViewPager;
+import com.rey.material.widget.SnackBar;
+import com.rey.material.widget.TabPageIndicator;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity implements ToolbarManager.OnToolbarGroupChangedListener{
+
+    private DrawerLayout dl_navigator;
+    private FrameLayout fl_drawer;
+    private ListView lv_drawer;
+    private CustomViewPager vp;
+    private TabPageIndicator tpi;
+
+    private PagerAdapter mPagerAdapter;
+
+    private Toolbar mToolbar;
+    private ToolbarManager mToolbarManager;
+    private SnackBar mSnackBar;
+    private Tab[] mItems = new Tab[]{Tab.MAPS, Tab.TABLE};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fl_drawer = (FrameLayout)findViewById(R.id.main_fl_drawer);
+        lv_drawer = (ListView)findViewById(R.id.main_lv_drawer);
+        mToolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        vp = (CustomViewPager)findViewById(R.id.main_vp);
+        tpi = (TabPageIndicator)findViewById(R.id.main_tpi);
+        dl_navigator = (DrawerLayout)findViewById(R.id.main_dl);
+        mSnackBar = (SnackBar)findViewById(R.id.main_sn);
+
+        mToolbarManager = new ToolbarManager(this, mToolbar, 0, R.style.ToolbarRippleStyle, R.anim.abc_fade_in, R.anim.abc_fade_out);
+        mToolbarManager.setNavigationManager(new ToolbarManager.BaseNavigationManager(R.style.NavigationDrawerDrawable, this, mToolbar, dl_navigator) {
+            @Override
+            public void onNavigationClick() {
+                if(mToolbarManager.getCurrentGroup() != 0)
+                    mToolbarManager.setCurrentGroup(0);
+                else
+                    dl_navigator.openDrawer(Gravity.START);
+            }
+
+            @Override
+            public boolean isBackState() {
+                return super.isBackState() || mToolbarManager.getCurrentGroup() != 0;
+            }
+
+            @Override
+            protected boolean shouldSyncDrawerSlidingProgress() {
+                return super.shouldSyncDrawerSlidingProgress() && mToolbarManager.getCurrentGroup() == 0;
+            }
+
+        });
+        mToolbarManager.registerOnToolbarGroupChangedListener(this);
+
+        mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), mItems);
+        vp.setAdapter(mPagerAdapter);
+        tpi.setViewPager(vp);
+        tpi.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                mSnackBar.dismiss();
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_le_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        mToolbarManager.onPrepareMenu();
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.tb_contextual:
+                mToolbarManager.setCurrentGroup(R.id.tb_group_contextual);
+                break;
+            case R.id.tb_done:
+            case R.id.tb_done_all:
+                mToolbarManager.setCurrentGroup(0);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onToolbarGroupChanged(int i, int i2) {
+        mToolbarManager.notifyNavigationStateChanged();
+    }
+
+    public SnackBar getSnackBar(){
+        return mSnackBar;
+    }
+
+    public enum Tab {
+        MAPS("Maps"),
+        TABLE("Table");
+        private final String name;
+
+        private Tab(String s) {
+            name = s;
+        }
+
+        public boolean equalsName(String otherName){
+            return (otherName != null) && name.equals(otherName);
+        }
+
+        public String toString(){
+            return name;
+        }
+
+    }
+
+    private static class PagerAdapter extends FragmentStatePagerAdapter {
+
+        Fragment[] mFragments;
+        Tab[] mTabs;
+
+        private static final Field sActiveField;
+
+        static {
+            Field f = null;
+            try {
+                Class<?> c = Class.forName("android.support.v4.app.FragmentManagerImpl");
+                f = c.getDeclaredField("mActive");
+                f.setAccessible(true);
+            } catch (Exception e) {}
+
+            sActiveField = f;
+        }
+
+        public PagerAdapter(FragmentManager fm, Tab[] tabs) {
+            super(fm);
+            mTabs = tabs;
+            mFragments = new Fragment[mTabs.length];
+
+
+            //dirty way to get reference of cached fragment
+            try{
+                ArrayList<Fragment> mActive = (ArrayList<Fragment>)sActiveField.get(fm);
+                if(mActive != null){
+                    for(Fragment fragment : mActive){
+                        if(fragment instanceof GeoMapFragment)
+                            setFragment(Tab.MAPS, fragment);
+                        else if(fragment instanceof TableFragment)
+                            setFragment(Tab.TABLE, fragment);
+                    }
+                }
+            }
+            catch(Exception e){}
+        }
+
+        private void setFragment(Tab tab, Fragment f){
+            for(int i = 0; i < mTabs.length; i++)
+                if(mTabs[i] == tab){
+                    mFragments[i] = f;
+                    break;
+                }
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(mFragments[position] == null){
+                switch (mTabs[position]) {
+                    case MAPS:
+                        mFragments[position] = GeoMapFragment.newInstance();
+                        break;
+                    case TABLE:
+                        mFragments[position] = TableFragment.newInstance();
+                        break;
+                }
+            }
+
+            return mFragments[position];
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTabs[position].toString().toUpperCase();
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.length;
+        }
+    }
+}
